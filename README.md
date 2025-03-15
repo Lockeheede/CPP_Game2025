fee5f64a8e87da46f55319f4ab8b485abba8e24a
Prescott Lawani <38223423+Lockeheede@users.noreply.github.com>
2025-03-15 17:08:54 -0500

    init remove
    -->Removing messed up files.

--- README.md
+++
@@ -1,2 +0,0 @@
-# CPP_Game2025
-Stephen Ulibarii (+ Enhancements!) To Make A Project In Unreal Engine 5.5 
--- Source/CPP_Game2025/CPP_Game2025.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "CPP_Game2025.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CPP_Game2025, "CPP_Game2025" );

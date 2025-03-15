3bfc300ab44ea019d4178fd3c725bba81537b5e2
Prescott Lawani <38223423+Lockeheede@users.noreply.github.com>
2025-03-15 16:22:45 -0500

    Laptop Update 03152025
    -->Moved project files up one directory so the ignores can work.

---
+++ Source/CPP_Game2025/CPP_Game2025.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CPP_Game2025.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CPP_Game2025, "CPP_Game2025" );
